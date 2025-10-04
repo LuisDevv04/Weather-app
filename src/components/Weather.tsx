4bfbaff605f1dc92b79621708a477c3e9e088c6b
LuisDevs <laldana161@gmail.com>
2025-10-03 20:14:07 -0600

    first commit

---
+++ src/components/Weather.tsx
@@ -0,0 +1,7 @@
+export default function Weather() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-blue-600 p-6 text-white">
+      <h1 className="text-3xl font-bold mb-6">🌤️Weather in your city</h1>
+    </div>
+  );
+}
